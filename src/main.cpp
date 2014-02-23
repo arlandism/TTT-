d97187bb2e66ca77cf57395dc83016b99da40ab6
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-02-23 13:44:55 -0600

    Remove dummy code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
-//
-//  main.cpp
-//  TTT++
-//
-//  Created by Arlandis Lawrence on 2/23/14.
-//  Copyright (c) 2014 Arlandis Lawrence. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
