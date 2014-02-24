2163f40573a177b58895e11d6aee35fbc07d9974
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-02-24 11:52:59 -0600

    Refactor tests

--- tests/playerfactorytest.cpp
+++ tests/playerfactorytest.cpp
@@ -2,18 +2,21 @@
 #include "mockmovestrategy.h"
 #include "playerfactory.h"
 
-TEST_CASE("PlayerFactory generates players using supplied token" "[playerfactory]"){
+TEST_CASE("PlayerFactory generates players..." "[playerfactory]"){
+    
     std::vector<int> inputsToReturn = {1};
     MoveStrategy *strategy = new MockMoveStrategy(inputsToReturn);
-    std::string token = "X";
-    Player *player = PlayerFactory::Create(strategy, token);
-    REQUIRE("X" == player->token());
-}
-
-TEST_CASE("PlayerFactory generates players using supplied move strategy" "[playerfactory]"){
-    std::vector<int> inputsToReturn = {1};
-    MoveStrategy *strategy = new MockMoveStrategy(inputsToReturn);
-    std::string token = "X";
-    Player *player = PlayerFactory::Create(strategy, token);
-    REQUIRE(1 == player->Move());
+    
+    SECTION("using supplied token"){
+        std::string token = "X";
+        Player *player = PlayerFactory::Create(strategy, token);
+        REQUIRE("X" == player->token());
+    }
+    
+    SECTION("using supplied move strategy"){
+        std::string token = "X";
+        Player *player = PlayerFactory::Create(strategy, token);
+        REQUIRE(1 == player->Move());
+    }
+    
 }
