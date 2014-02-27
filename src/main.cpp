6eea42c2c387dc72c258cfb461f0dc00e64041ff
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-02-27 16:05:06 -0600

    More realistic prototype of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "vector"
-
-std::string first_player_type, second_player_type,
-            first_player_token, second_player_token;
-
-std::vector<std::string> valid_player_choices = {"Human", "AI"};
+#include "game.h"
+#include "player.h"
+#include "movestrategy.h"
+#include "minimaxstrategy.h"
+#include "tttsettings.h"
+#include "userinputstrategy.h"
 
 void PrintWelcomeBanner(){
     std::cout << "***************************\n";
@@ -12,7 +12,60 @@ void PrintWelcomeBanner(){
     std::cout << "***************************\n";
 }
 
+void PlayerPrompt(){
+    std::cout << "What type of player would you like to be?\n";
+}
+
+void TokenPrompt(){
+    std::cout << "What's that player's token?\n";
+}
+
+void PrintWinner(TTTRules rules){
+    std::cout << "And the winner is... " << rules.GameWinner() << "\n";
+}
+
+Player * CreatePlayer(){
+    std::string player_type;
+    std::string player_token;
+    
+    PlayerPrompt();
+    std::cin >> player_type;
+    TokenPrompt();
+    std::cin >> player_token;
+    
+    MoveStrategy *move_strategy;
+    if (player_type == "human"){
+        move_strategy = new UserInputStrategy();
+    } else {
+        move_strategy = new MinimaxStrategy(new TTTSettings("x","o"));
+    }
+    return new Player(player_token, move_strategy);
+}
+
+void StartGame(){
+    PrintWelcomeBanner();
+    Player *player_one = CreatePlayer();
+    Player *player_two = CreatePlayer();
+    Board *board = new Board(3);
+    Game game = Game(board, player_one, player_two);
+    TTTRules rules = *new TTTRules(board, player_one->token(), player_two->token());
+    while (not (rules.GameOver())){
+        game.Round();
+    }
+    PrintWinner(rules);
+}
+
+void PromptForReplay(){
+    std::cout << "Would you like to play again?\n";
+    std::string answer;
+    std::cin >> answer;
+    if (answer == "y"){
+        StartGame();
+    }
+}
+
 int main(int argc, const char * argv[])
 {
+    StartGame();
     return 0;
 }
