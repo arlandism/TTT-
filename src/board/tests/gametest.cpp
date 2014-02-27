47ebe1526b7d0cd09bb5598bf310fe4c1f02ff43
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-02-27 15:25:23 -0600

    Refactor Game tests

--- src/board/tests/gametest.cpp
+++ src/board/tests/gametest.cpp
@@ -1,27 +1,25 @@
 #include "catch.hpp"
 #include "board.h"
 #include "game.h"
-#include "player.h"
 #include "mockplayer.h"
 
-TEST_CASE("Game calls Move on player one with board"){
-    Board *board = new Board(3);
+TEST_CASE("Game calls Move on..."){
+    
     std::vector<int> player_one_moves = {1};
     std::vector<int> player_two_moves = {2};
     IPlayer *player_one = new MockPlayer("x", player_one_moves);
     IPlayer *player_two = new MockPlayer("o", player_two_moves);
-    Game game = *new Game(board, player_one, player_two);
-    game.Round();
-    REQUIRE("x" == board->state()[0]);
-}
-
-TEST_CASE("Game calls Move on player two with board"){
     Board *board = new Board(3);
-    std::vector<int> player_one_moves = {1};
-    std::vector<int> player_two_moves = {2};
-    IPlayer *player_one = new MockPlayer("x", player_one_moves);
-    IPlayer *player_two = new MockPlayer("o", player_two_moves);
-    Game game = *new Game(board, player_one, player_two);
-    game.Round();
-    REQUIRE("o" == board->state()[1]);
+    
+    SECTION("player one with board"){
+        Game game = *new Game(board, player_one, player_two);
+        game.Round();
+        REQUIRE("x" == board->state()[0]);
+    }
+    
+    SECTION("player two with board"){
+        Game game = *new Game(board, player_one, player_two);
+        game.Round();
+        REQUIRE("o" == board->state()[1]);
+    }
 }
