4fc6676931b47fd70579e291c84c40ed9096878d
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-03-03 13:34:36 -0600

    Remove code from top of gametest file

--- src/game/tests/gametest.cpp
+++ src/game/tests/gametest.cpp
@@ -3,12 +3,6 @@
 #include "game.h"
 #include "mockplayer.h"
 
-std::vector<int> player_one_moves = {1};
-std::vector<int> player_two_moves = {2};
-IPlayer *player_one = new MockPlayer("x", player_one_moves);
-IPlayer *player_two = new MockPlayer("o", player_two_moves);
-Board *board = new Board(3);
-
 TEST_CASE("Game::Round calls Move on..."){
     
     std::vector<int> player_one_moves = {1};
@@ -28,9 +22,14 @@ TEST_CASE("Game::Round calls Move on..."){
         game.Round();
         REQUIRE("o" == board->state()[1]);
     }
+    
 }
 
 TEST_CASE("Game::Summary displays board"){
+    std::vector<int> player_one_moves = {};
+    std::vector<int> player_two_moves = {};
+    IPlayer *player_one = new MockPlayer("x", player_one_moves);
+    IPlayer *player_two = new MockPlayer("o", player_two_moves);
     std::vector<std::string> board_state = {"x", "o", "x", "", "", "", "", "", ""};
     Board *board = new Board(board_state);
     Game game = Game(board, player_one, player_two);
