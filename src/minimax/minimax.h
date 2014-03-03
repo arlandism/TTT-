855fae24ae7804242f6fc5cb506b3b84a98b780a
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-03-03 10:13:36 -0600

    Refactoring Minimax

--- src/minimax/minimax.h
+++ src/minimax/minimax.h
@@ -18,16 +18,18 @@ public:
     int NextMove(Board);
 
 private:
+    
     const int MAX_DEPTH = 20;
+    std::string token_;
+    
     std::multimap<int, int> ScorePossibleMoves(Board);
+    int HighestRatedMove(std::multimap<int, int>);
     std::string OtherToken(std::string);
     int ScoreMove(Board, int, std::string, int depth=1, bool maximizing=true);
     int Maximize(Board, std::string, int);
     int Minimize(Board, std::string, int);
     std::string GameWinner(Board);
     bool GameOver(Board);
-    int HighestRatedMove(std::multimap<int, int>);
     int EvaluateGame(std::string, std::string, int);
-    std::string token_;
 };
 #endif
