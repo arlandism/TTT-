ac8f6ac5c424915e0f47e12bd8dd931d1e12651c
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-03-04 11:28:18 -0600

    Extract another method in 'TTTEngine'

--- src/tttlogic/tttengine.cpp
+++ src/tttlogic/tttengine.cpp
@@ -1,17 +1,12 @@
 #include "tttengine.h"
 
 void TTTEngine::StartGame(){
-    std::string second_player_type,
-                second_player_token,
-                play_again_answer;
     
     driver_->DisplayWelcome();
     IPlayer *player_one = CreatePlayerOne();
     std::string first_player_token = player_one->token();
     
-    second_player_type = driver_->GetPlayerType("second", factory_->ValidPlayerChoices());
-    second_player_token = TTTRules::OtherToken(first_player_token);
-    IPlayer *player_two = CreatePlayer(second_player_type, second_player_token);
+    IPlayer *player_two = CreatePlayerTwo(first_player_token);
     
     Board board = *new Board(3);
     Game game = *new Game(&board, player_one, player_two);
@@ -32,4 +27,11 @@ IPlayer * TTTEngine::CreatePlayerOne(){
     std::string player_token = driver_->GetToken(TTTRules::ValidTokenChoices());
     IPlayer *player_one = CreatePlayer(player_type, player_token);
     return player_one;
+}
+
+IPlayer * TTTEngine::CreatePlayerTwo(std::string first_player_token){
+    std::string player_type = driver_->GetPlayerType("second", factory_->ValidPlayerChoices());
+    std::string player_token = TTTRules::OtherToken(first_player_token);
+    IPlayer *player_two = CreatePlayer(player_type, player_token);
+    return player_two;
 }
--- src/tttlogic/tttengine.h
+++ src/tttlogic/tttengine.h
@@ -20,6 +20,7 @@ public:
     void StartGame();
     IPlayer * CreatePlayer(std::string, std::string);
     IPlayer * CreatePlayerOne();
+    IPlayer * CreatePlayerTwo(std::string);
     
 private:
     UIDriver *driver_;
