07a44033c46debaad27acad29210d5b8f2c0e130
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-03-04 21:08:47 -0600

    Refactor minimaxtest

--- src/game/tests/minimaxtest.cpp
+++ src/game/tests/minimaxtest.cpp
@@ -7,27 +7,19 @@ TEST_CASE("Minimax::NextMove..."){
     Minimax minimax = *new Minimax("x");
 
     SECTION("goes for immediate win"){
-        Board board = *new Board(3);
-        board.Move(3, "x");
-        board.Move(6, "x");
-        int winning_move = 9;
+        std::vector<std::string> board_state = {"x", "x", "",
+                                                "", "o", "o",
+                                                "", "", ""};
+        Board board = *new Board(board_state);
+        int winning_move = 3;
         REQUIRE(winning_move == minimax.NextMove(board));
-    }
-
-    SECTION("blocks a move"){
-        Board board = *new Board(3);
-        board.Move(1, "o");
-        board.Move(3, "x");
-        board.Move(4, "o");
-        int blocking_move = 7;
-        REQUIRE(blocking_move == minimax.NextMove(board));
     }
     
     SECTION("anticipates a fork"){
-        Board board = *new Board(3);
-        board.Move(1, "o");
-        board.Move(2, "x");
-        board.Move(3, "o");
+        std::vector<std::string> board_state = {"o", "x", "o",
+                                                "", "", "",
+                                                "", "", ""};
+        Board board = *new Board(board_state);
         int blocking_move = 5;
         REQUIRE(blocking_move == minimax.NextMove(board));
     }
